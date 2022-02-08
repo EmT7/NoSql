29be7ba9f4eca2a3f2a22758ec081d68f2231a84
Miranda <ranileah7@gmail.com>
2022-02-07 19:12:47 -0800

    working APIs

--- models/index.js
+++ models/index.js
@@ -2,4 +2,4 @@ const User = require("./User");
 
 const Thought = require("./Thought");
 
-module.exports = { user, thought };
+module.exports = { User, Thought };
---
+++ routes/api/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const userRoutes = require("./user-routes");
+const thoughtRoutes = require("./thought-routes");
+
+router.use("/users", userRoutes);
+
+router.use("/thoughts", thoughtRoutes);
+
+router.use((req, res) => {
+    return res.send("Error");
+  });
+
+module.exports = router;
